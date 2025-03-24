985154589ba5d5a135f2aa414b5511b566f18f93
alzate25 <131006522+alzate25@users.noreply.github.com>
2025-03-24 18:19:14 +0000

    Componentes cargados

---
+++ actividadReactBase/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Estado from './Estado'
+import String from './String'
+import Booleano from './Boolean'
+import Objeto from './Objeto'
+import Clase from './Clase'
+function App() {
+  return (
+    <>
+      <Estado />
+      <String />
+      <Booleano />
+      <Objeto />
+      <Clase />
+    </>
+  )
+}
+
+export default App
